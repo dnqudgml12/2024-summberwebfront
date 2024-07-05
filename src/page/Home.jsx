3b5834f1edce85e9a9106123029155d38a8e4597
우병희 <ubyeonghui@ubyeonghuiui-MacBookPro.local>
2024-07-05 20:59:03 +0900

    addnolinkjustdiv

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,18 +1,110 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
+import Homenavbar from "../components/Home/Homenavbar";
+import { Data } from "../data/freeBoard";
+import { Datasss } from "../data/secretBoard";
+import { Datass } from "../data/gradutateBoard";
+import {Alldiv,Bodydiv,Boardalldiv,Eachboard,Eachboardword,Eachseperateboard} from "../styles/HomeStyled";
+import mypic from "../assets/img/mypicture.jpeg";
+const Home = () => {
+  return (
+    <Alldiv>
 
-const Home=()=>{
+      <Bodydiv>
+        <Boardalldiv>
+          <Eachboard align={"center"} border={"1px solid gray"}width={"300px"}>
+          <Myprofile src={mypic} />
+          <Myname>우병희 학부생</Myname>
+          <Myemail>dnqudgml@gmail.com</Myemail>
+          <Mywriting>내가 쓴글</Mywriting>
+          <Mycomment>댓글 단글</Mycomment>
+          </Eachboard>
+          <Eachboard padle={"70px"}>
+          <Eachboardword >자유게시판</Eachboardword>
+            {Data.slice(0,8).map((post) => (
+              
+                <Eachseperateboard to = {`/freeboard/${post.id}`} >
+                  {post.title}</Eachseperateboard>
+           
+            ))}
+          </Eachboard>
+          <Eachboard>
+          <Eachboardword>비밀 게시판</Eachboardword>
+            {Datasss.slice(0,8).map((post) => (
+              
+                <Eachseperateboard to = {`/secreteboard/${post.id}`} >
+                  {post.title}</Eachseperateboard>
+           
+            ))}
+          </Eachboard>
 
+          <Eachboard>
+          <Eachboardword>졸업 게시판</Eachboardword>
+            {Datass.slice(0,8).map((post) => (
+              
+                <Eachseperateboard to = {`/graduateboard/${post.id}`} >
+                  {post.title}</Eachseperateboard>
+           
+            ))}
+          </Eachboard>
+        </Boardalldiv>
+      </Bodydiv>
+    </Alldiv>
+  );
+};
 
-    return( <div>
-        <h1>게시판 선택</h1>
-        <ul>
-          <li><Link to="/freeboard">자유 게시판</Link></li>
-          <li><Link to="/secreteboard">비밀 게시판</Link></li>
-          <li><Link to="/graduateboard">졸업 게시판</Link></li>
-        </ul>
-      </div>)
-}
+const Myprofile= styled.img`
+
+  padding-top: 30px;
+  width: 200px;
+  height: 150px;
+
+`
+
+const Myname= styled.div`
+
+  margin-top: 30px;
+   width: 120px;
+   height: 30px;
+   font-size:20px;
+
+`
+
+const Myemail= styled.div`
+
+  margin-top: 10px;
+  padding-left: -20px;
+   width: auto;
+   height: 50px;
+   font-size:20px;
+
+`
+const Mywriting= styled(Link)`
+    margin-top: 30px;
+   width: 120px;
+   height: 30px;
+   font-size:20px;
+   padding-top: 7px;
+   &:hover{
+    background-color: gray;
+   }
+   text-decoration: none;
+   border: 1px solid gray;
+
+`
+const Mycomment= styled(Link)`
+    margin-top: 30px;
+   width: 120px;
+   height: 30px;
+   font-size:20px;
+   padding-top: 7px;
+   &:hover{
+    background-color: gray;
+   }
+   text-decoration: none;
+   border: 1px solid gray;
+
+`
 
 export default Home;
