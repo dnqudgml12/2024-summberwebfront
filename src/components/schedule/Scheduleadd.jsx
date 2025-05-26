34e413b04da3b7b8bc8bdbb0a9a95aa1666ca394
dnqudgml12 <21800454@handong.ac.kr>
2025-05-26 23:56:51 +0900

    sorry

--- src/components/schedule/Scheduleadd.jsx
+++ src/components/schedule/Scheduleadd.jsx
@@ -1,244 +1,234 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { TimetableData } from "../../data/schedule";
 import {
-  Adddiv,
-  HoverMenu,
-  ModalBackground,
-  ModalContent,
-  DaySelector,
-  DayButton,
-  TimeSelector,
-  ButtonGroup,
-  Form,
-  Title,
-  Subjectdiv,
-  Subjectname,
-  Subjectinput,
-  Professordiv,
-  Professorname,
-  Professorinput,
-  Timeandplacediv,
-  Timeandplacename,
-  TimeandplaceinputDiv,
-  StarttimeSelect,
-  EndtimeSelect,
-  Placeinput,
-  Canclebutton,
-  Canclediv,
-  Savebutton,
+	Adddiv,
+	HoverMenu,
+	ModalBackground,
+	ModalContent,
+	DaySelector,
+	DayButton,
+	TimeSelector,
+	ButtonGroup,
+	Form,
+	Title,
+	Subjectdiv,
+	Subjectname,
+	Subjectinput,
+	Professordiv,
+	Professorname,
+	Professorinput,
+	Timeandplacediv,
+	Timeandplacename,
+	TimeandplaceinputDiv,
+	StarttimeSelect,
+	EndtimeSelect,
+	Placeinput,
+	Canclebutton,
+	Canclediv,
+	Savebutton,
 } from "../../styles/Schedulestyled";
-import Cancle  from "../../assets/img/Cancle.png"
+import Cancle from "../../assets/img/Cancle.png";
 const AddModal = ({ onAdd, data, onCancel }) => {
-  const [subject, setSubject] = useState("");
-  const [professor, setProfessor] = useState("");
-  const [day, setDay] = useState("");
-  const [startTime, setStartTime] = useState("09:00");
-  const [endTime, setEndTime] = useState("10:00");
-  const [location, setLocation] = useState("");
-  const [error, setError] = useState("");
+	const [subject, setSubject] = useState("");
+	const [professor, setProfessor] = useState("");
+	const [day, setDay] = useState("");
+	const [startTime, setStartTime] = useState("09:00");
+	const [endTime, setEndTime] = useState("10:00");
+	const [location, setLocation] = useState("");
+	const [error, setError] = useState("");
 
-  const getRandomColor = () => {
-    const base = 255;
-    const mix = 200;
-    const getRandomValue = () => Math.floor(Math.random() * (base - mix) + mix);
-    const r = getRandomValue();
-    const g = getRandomValue();
-    const b = getRandomValue();
-    const color = `rgb(${r}, ${g}, ${b})`;
-    return color;
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault(); //렌더링을 막음 -> 렌더링 시에 값이 없어지므로
+	const getRandomColor = () => {
+		const base = 255;
+		const mix = 200;
+		const getRandomValue = () => Math.floor(Math.random() * (base - mix) + mix);
+		const r = getRandomValue();
+		const g = getRandomValue();
+		const b = getRandomValue();
+		const color = `rgb(${r}, ${g}, ${b})`;
+		return color;
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault(); //렌더링을 막음 -> 렌더링 시에 값이 없어지므로
 
-    console.log("Submitting new entry with the following details:");
-    console.log("Subject:", subject);
-    console.log("Professor:", professor);
-    console.log("Day:", day);
-    console.log("Start Time:", startTime);
-    console.log("End Time:", endTime);
-    console.log("Location:", location);
+		console.log("Submitting new entry with the following details:");
+		console.log("Subject:", subject);
+		console.log("Professor:", professor);
+		console.log("Day:", day);
+		console.log("Start Time:", startTime);
+		console.log("End Time:", endTime);
+		console.log("Location:", location);
 
-    // 시간 중복 체크
-    const isConflict = data.some((entry) => {
-      return (
-        entry.day === day &&
-        ((entry.startTime >= startTime && entry.startTime < endTime) ||
-          (entry.endTime > startTime && entry.endTime <= endTime) ||
-          (entry.startTime <= startTime && entry.endTime >= endTime))
-      );
-    });
+		// 시간 중복 체크
+		const isConflict = data.some((entry) => {
+			return (
+				entry.day === day &&
+				((entry.startTime >= startTime && entry.startTime < endTime) ||
+					(entry.endTime > startTime && entry.endTime <= endTime) ||
+					(entry.startTime <= startTime && entry.endTime >= endTime))
+			);
+		});
 
+		// 과목명, 교수명, 위치 중복 체크
+		const isDuplicate = data.some((entry) => {
+			return (
+				entry.subject === subject ||
+				entry.professor === professor ||
+				entry.location === location
+			);
+		});
 
-    // 과목명, 교수명, 위치 중복 체크
-    const isDuplicate = data.some((entry) => {
-      return (
-        entry.subject === subject ||
-        entry.professor === professor ||
-        entry.location === location
-      );
-    });
+		if (isConflict) {
+			alert("이미 시간이 정해진 곳에 추가할 수 없습니다.");
+			return;
+		} else if (isDuplicate) {
+			alert("과목명, 교수명 또는 위치가 이미 존재합니다.");
+			return;
+		} else {
+			const newEntry = {
+				id: data.length + 1,
+				subject,
+				professor,
+				day,
+				startTime,
+				endTime,
+				location,
+				color: getRandomColor(),
+			};
 
-    if (isConflict) {
-      alert("이미 시간이 정해진 곳에 추가할 수 없습니다.");
-      return;
-    } else if (isDuplicate) {
-      alert("과목명, 교수명 또는 위치가 이미 존재합니다.");
-      return;
-    } else {
-      const newEntry = {
-        id: data.length + 1,
-        subject,
-        professor,
-        day,
-        startTime,
-        endTime,
-        location,
-        color: getRandomColor(),
-      };
+			onAdd(newEntry);
+			setSubject("");
+			setProfessor("");
+			setDay("");
+			setStartTime("09:00");
+			setEndTime("10:00");
+			setLocation("");
+			setError("");
+		}
+	};
 
-      onAdd(newEntry);
-      setSubject("");
-      setProfessor("");
-      setDay("");
-      setStartTime("09:00");
-      setEndTime("10:00");
-      setLocation("");
-      setError("");
-    }
-  };
+	const timeOptions = [
+		"09:00",
+		"10:00",
+		"11:00",
+		"12:00",
+		"13:00",
+		"14:00",
+		"15:00",
+		"16:00",
+		"17:00",
+		"18:00",
+		"19:00",
+		"20:00",
+	];
 
-  const timeOptions = [
-    "09:00",
-    "10:00",
-    "11:00",
-    "12:00",
-    "13:00",
-    "14:00",
-    "15:00",
-    "16:00",
-    "17:00",
-    "18:00",
-    "19:00",
-    "20:00",
-  ];
+	const filteredEndTimes = timeOptions.filter((time) => time > startTime);
+	const days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+	const displayDays = ["월", "화", "수", "목", "금"];
+	console.log("Filtered End Times:", filteredEndTimes);
 
-  const filteredEndTimes = timeOptions.filter((time) => time > startTime);
-  const days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
-  const displayDays = ["월", "화", "수", "목", "금"];
+	// useEffect를 사용하여 startTime이 변경될 때 endTime을 자동으로 업데이트
+	useEffect(() => {
+		if (startTime >= endTime) {
+			setEndTime(filteredEndTimes.length > 0 ? filteredEndTimes[0] : "");
+		}
+	}, [startTime]);
 
-    // useEffect를 사용하여 startTime이 변경될 때 endTime을 자동으로 업데이트
-    useEffect(() => {
-      if (startTime >= endTime) {
-        setEndTime(filteredEndTimes.length > 0 ? filteredEndTimes[0] : "");
-      }
-    }, [startTime]);
-  
-  return (
-   
-    <ModalBackground>
-      
-      <ModalContent>
-      <Canclediv onClick={onCancel} >
-          <Canclebutton src={Cancle} />
-        </Canclediv>
-        <Form onSubmit={handleSubmit}>
-   
-        
- 
-          <Title>새 수업 추가</Title>
-          <Subjectdiv>
-            <Subjectname>과목명 (필수)</Subjectname>
-            <Subjectinput
-              type="text"
-              value={subject}
-              onChange={(e) => setSubject(e.target.value)}
-              placeholder="예) 경제학입문"
-              required
-            />
-          </Subjectdiv>
-          <Professordiv>
-            <Professorname>교수명 (필수)</Professorname>
-            <Professorinput
-              type="text"
-              value={professor}
-              onChange={(e) => setProfessor(e.target.value)}
-              placeholder="예) 홍길동"
-              required
-            />
-          </Professordiv>
+	return (
+		<ModalBackground>
+			<ModalContent>
+				<Canclediv onClick={onCancel}>
+					<Canclebutton src={Cancle} />
+				</Canclediv>
+				<Form onSubmit={handleSubmit}>
+					<Title>새 수업 추가</Title>
+					<Subjectdiv>
+						<Subjectname>과목명 (필수)</Subjectname>
+						<Subjectinput
+							type="text"
+							value={subject}
+							onChange={(e) => setSubject(e.target.value)}
+							placeholder="예) 경제학입문"
+							required
+						/>
+					</Subjectdiv>
+					<Professordiv>
+						<Professorname>교수명 (필수)</Professorname>
+						<Professorinput
+							type="text"
+							value={professor}
+							onChange={(e) => setProfessor(e.target.value)}
+							placeholder="예) 홍길동"
+							required
+						/>
+					</Professordiv>
 
-          <Timeandplacediv>
-            <Timeandplacename>시간/장소</Timeandplacename>
-            <TimeandplaceinputDiv>
-              <DaySelector>
-                {days.map((d, index) => (
-                  <DayButton
-                    key={d}
-                    active={day === d}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      setDay(d);
-                    }}
-                  >
-                    {displayDays[index]}
-                  </DayButton>
-                ))}
-              </DaySelector>
+					<Timeandplacediv>
+						<Timeandplacename>시간/장소</Timeandplacename>
+						<TimeandplaceinputDiv>
+							<DaySelector>
+								{days.map((d, index) => (
+									<DayButton
+										key={d}
+										active={day === d}
+										onClick={(e) => {
+											e.preventDefault();
+											setDay(d);
+										}}
+									>
+										{displayDays[index]}
+									</DayButton>
+								))}
+							</DaySelector>
 
-              <TimeSelector>
-                <StarttimeSelect
-                  value={startTime}
-                  onChange={(e) =>{
-                    setStartTime(e.target.value);
-                  
-                  /*
+							<TimeSelector>
+								<StarttimeSelect
+									value={startTime}
+									onChange={(e) => {
+										setStartTime(e.target.value);
+
+										/*
                     setEndTime(
                       filteredEndTimes.length > 0 ? filteredEndTimes[0] : ""
                     );
                     //starttimeSelect'에 대한 'onChange' 핸들러를 업데이트하여 'endTime'을 선택한 'startTime' 이후 사용 가능한 첫 번째 옵션으로 자동 설정
                   */
-                    }
+									}}
+								>
+									{timeOptions.map((time) => (
+										<option key={time} value={time}>
+											{time}
+										</option>
+									))}
+								</StarttimeSelect>
+								~
+								<EndtimeSelect
+									value={endTime}
+									onChange={(e) => setEndTime(e.target.value)}
+								>
+									{filteredEndTimes.map((times) => (
+										<option key={times} value={times}>
+											{times}
+										</option>
+									))}
+								</EndtimeSelect>
+								<Placeinput
+									type="text"
+									value={location}
+									onChange={(e) => setLocation(e.target.value)}
+									placeholder="예)종303"
+								/>
+							</TimeSelector>
+						</TimeandplaceinputDiv>
+					</Timeandplacediv>
 
-                    
-                  }
-                >
-                  {timeOptions.map((time) => (
-                    <option key={time} value={time}>
-                      {time}
-                    </option>
-                  ))}
-                </StarttimeSelect>
-                ~
-                <EndtimeSelect
-                  value={endTime}
-                  onChange={(e) => setEndTime(e.target.value)}
-                >
-                  {filteredEndTimes.map((times) => (
-                    <option key={times} value={times}>
-                      {times}
-                    </option>
-                  ))}
-                </EndtimeSelect>
-                <Placeinput
-                  type="text"
-                  value={location}
-                  onChange={(e) => setLocation(e.target.value)}
-                  placeholder="예)종303"
-                />
-              </TimeSelector>
-            </TimeandplaceinputDiv>
-          </Timeandplacediv>
-
-          <ButtonGroup>
-            
-            <Savebutton type="submit">저장</Savebutton>
-     
-          </ButtonGroup>
-        </Form>
-      </ModalContent>
-    </ModalBackground>
-  );
+					<ButtonGroup>
+						<Savebutton type="submit">저장</Savebutton>
+					</ButtonGroup>
+				</Form>
+			</ModalContent>
+		</ModalBackground>
+	);
 };
 
 export default AddModal;
